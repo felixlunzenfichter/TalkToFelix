49f820704aa6ecd96ac7fefe652106be1bb8c48a
Felix Lunzenfichter <felixlunzenfichter@gmail.com>
2021-10-14 17:19:26 -0300

    Initial Commit

---
+++ TalkToFelix/TalkToFelixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkToFelixApp.swift
+//  TalkToFelix
+//
+//  Created by Felix Lunzenfichter on 14.10.21.
+//
+
+import SwiftUI
+
+@main
+struct TalkToFelixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
