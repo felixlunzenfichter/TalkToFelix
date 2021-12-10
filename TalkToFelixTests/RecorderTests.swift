dfa3a0d23ab8adff783d2a2663f08bf21f4a3fd4
Felix Lunzenfichter <felixlunzenfichter@gmail.com>
2021-12-10 06:02:52 +0100

    Enforce independence of tests by clearing audio file.

--- TalkToFelixTests/RecorderTests.swift
+++ TalkToFelixTests/RecorderTests.swift
@@ -11,35 +11,47 @@ import XCTest
 class RecorderTests: XCTestCase {
     
     let halfASecond: UInt32 = 500000
-
+    
     func testLengthOfRecordingIsZeroAtStart() {
         let recorder: Recorder = MyRecorder()
         XCTAssertEqual(recorder.length, 0)
     }
-
+    
     func testRecordForHalfASecond() {
         let recorder: Recorder = MyRecorder()
-
+        
         recorder.start()
         usleep(halfASecond)
         let recordingLength = recorder.length
-                
+        
         XCTAssertGreaterThan(recordingLength, 0.4)
         XCTAssertLessThan(recordingLength, 0.6)
     }
     
     func testRecordThenStopThenRecordForHalfASecond() {
         let recorder: Recorder = MyRecorder()
-
+        
         recorder.start()
         usleep(halfASecond)
         recorder.stop()
         recorder.start()
         usleep(halfASecond)
         let recordingLength = recorder.length
-                
+        
         XCTAssertGreaterThan(recordingLength, 0.4)
         XCTAssertLessThan(recordingLength, 0.6)
+    }
+    
+    override class func tearDown() {
+        let recorder = MyRecorder()
+        let filename = recorder.audioFilename
+        try? FileManager.default.removeItem(atPath: filename.path)
+        
+    }
+    override func setUp() {
+        let recorder = MyRecorder()
+        let filename = recorder.audioFilename
+        try? FileManager.default.removeItem(atPath: filename.path)
     }
     
 }
